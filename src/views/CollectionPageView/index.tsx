5cf113a41781519cf7393b0aa6b3cc7b400732be
TanninOne <seppleviathan@gmx.de>
2023-01-27 09:56:30 +0100

    allow filtering mod list by  "Not installed"

--- src/views/CollectionPageView/index.tsx
+++ src/views/CollectionPageView/index.tsx
@@ -231,6 +231,7 @@ class CollectionPage extends ComponentEx<IProps, IComponentState> {
           { value: 'Installing', label: 'Installing' },
           { value: 'Downloading', label: 'Downloading' },
           { value: 'Pending', label: 'Pending' },
+          { value: 'Recommended', label: 'Not installed' },
           { value: 'Ignored', label: 'Ignored' },
         ], true, false),
       },
