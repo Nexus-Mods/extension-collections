6d616c2f426779b9bccc5541258325ed1db148be
TanninOne <seppleviathan@gmx.de>
2023-05-25 10:49:15 +0200

    performance improvements for mod table

--- src/collectionCreate.ts
+++ src/collectionCreate.ts
@@ -1,3 +1,4 @@
+import memoizeOne from 'memoize-one';
 import { MOD_TYPE } from './constants';
 import { createCollectionFromProfile } from './util/transformCollection';
 
@@ -31,41 +32,34 @@ export async function initFromProfile(api: types.IExtensionApi, profileId: strin
   }
 }
 
+const collections = memoizeOne((mods: { [modId: string]: types.IMod }) => {
+  const isWorkshopCollection = mod => (mod.type === MOD_TYPE)
+          && (mod.attributes?.editable === true);
+  return Object.values(mods)
+    .filter(isWorkshopCollection)
+    .map(coll => new Set((coll.rules ?? []).map(rule => rule.reference.id)));
+});
+
 export function addCollectionCondition(api: types.IExtensionApi, instanceIds: string[]) {
   const state = api.getState();
   const gameId = selectors.activeGameId(state);
   const mods = state.persistent.mods[gameId];
-  return Object.keys(mods)
-    .find(collectionId => {
-      if ((mods[collectionId].type !== MOD_TYPE)
-          || (mods[collectionId].attributes?.editable !== true)) {
-        return false;
-      }
 
-      const rules = mods[collectionId].rules ?? [];
-
-      // only offer the option if there is at least one mod assigned to a collection
-      return instanceIds.find(modId =>
-        !alreadyIncluded(rules, modId) && (mods[modId].type !== MOD_TYPE)) !== undefined;
-    }) !== undefined;
+  return collections(mods).find(ruleSet => {
+    // only offer the option if there is at least one mod not assigned to a collection
+    return instanceIds.find(modId =>
+      (mods[modId].type !== MOD_TYPE) && !ruleSet.has(modId)) !== undefined;
+  }) !== undefined;
 }
 
 export function removeCollectionCondition(api: types.IExtensionApi, instanceIds: string[]) {
   const state = api.getState();
   const gameId = selectors.activeGameId(state);
   const mods = state.persistent.mods[gameId];
-  return Object.keys(mods)
-    .find(collectionId => {
-      if ((mods[collectionId].type !== MOD_TYPE)
-          || (mods[collectionId].attributes?.editable !== true)) {
-        return false;
-      }
-
-      const rules = mods[collectionId].rules ?? [];
-
-      // only offer the option if there is at least one mod assigned to a collection
-      return instanceIds.find(modId => alreadyIncluded(rules, modId)) !== undefined;
-    }) !== undefined;
+  return collections(mods).find(ruleSet => {
+    // only offer the option if there is at least one mod assigned to a collection
+    return instanceIds.find(modId => ruleSet.has(modId)) !== undefined;
+  }) !== undefined;
 }
 
 export function alreadyIncluded(rules, modId): boolean {
